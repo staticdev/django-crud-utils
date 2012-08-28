b5aa95f98c1c78b4446629cb753931648b467fc7
staticdev <thiagocavila@gmail.com>
2012-08-28 18:59:43 -0300

    Fixing dependency_links to setup.py

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
 #        'https://github.com/staticdev/django-sorting/tarball/master#eg‌​g=django-sort-123dev',
 #    ],
     install_requires=[
-        'Django>=1.3.1',
+#        'Django>=1.3.1',
         'django-pagination>=1.0.7',
 #        'django-sort==123dev',
     ],
