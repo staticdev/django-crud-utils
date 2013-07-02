4059c3f7a613aa78990eaa721b8d3b58c735db6e
staticdev <thiagocavila@gmail.com>
2013-07-01 23:50:29 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
 #    ],
     install_requires=[
         'Django >= 1.2.0',
-        'django-bootstrap-pagination',
+#        'django-bootstrap-pagination',
     ],
     classifiers = [
         'License :: OSI Approved :: MIT License',
